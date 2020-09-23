53341c00756a096ad4fcf588fe47c92029a6a349
Adam <adam.e.sheaffer@gmail.com>
2020-09-23 10:03:00 -0500

    remove passwords from nutshell

--- src/scripts/auth/LoginForm.js
+++ src/scripts/auth/LoginForm.js
@@ -8,19 +8,14 @@ eventHub.addEventListener("userAuthenticated", e => {
 eventHub.addEventListener("click", e => {
     if (e.target.id === "login__button") {
         const username = document.querySelector("#login__username").value
-        const password = document.querySelector("#login__password").value
-
 
         return fetch(`http://localhost:8088/users?username=${username}`)
             .then(response => response.json())
             .then(users => {
                 if (users.length > 0) {
                     const user = users[0]
-
-                    if (user.password === password) {
-                        sessionStorage.setItem("activeUser", user.id)
-                        eventHub.dispatchEvent(new CustomEvent("userAuthenticated"))
-                    }
+                    sessionStorage.setItem("activeUser", user.id)
+                    eventHub.dispatchEvent(new CustomEvent("userAuthenticated"))
                 }
             })
     }
@@ -31,8 +26,6 @@ const render = () => {
     contentTarget.innerHTML += `
         <section class="login">
             <input id="login__username" type="text" placeholder="Enter your username">
-            <input id="login__password" type="password" placeholder="Enter your password">
-
             <button id="login__button">Log In</button>
         </section>
     `
